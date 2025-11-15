ba82c6d4ac4e0062f324c18599a7cc7b4cc70b0a
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-11-15 19:01:27 -0300

    estudos

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -19,3 +19,25 @@ espera.insert(2,"lobo")
 espera_texto = "-".join(espera)
 
 print(espera_texto)
+#PRATICA DA BUDEGA
+
+class Pessoa:
+    def __init__(self, nome :str):
+        self.nome = nome
+    def __str__(self):
+        return self.nome
+    
+class budega:
+    def __init__(self, num_caixas :int):
+        self.caixas: list[Pessoa | None] = []
+        for _ in range (num_caixas):
+            self.caixas.append(None)
+        self.espera: list[Pessoa] =[]
+    def __str__(self):
+        return f"Caixas:{num_caixas
+
+        pessoa = Pessoa("Maria")
+        print("maria")
+
+        budega = Budega(3)
+        print(budega)

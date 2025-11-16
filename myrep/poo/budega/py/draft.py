21e1a6f3a98a97707d60b180c4319c77376010de
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-11-16 15:15:49 -0300

    estudando domingo

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,31 +1,43 @@
-#PRATICA DA BUDEGA
-
 class Pessoa:
-    def __init__(self, nome :str):
+    def __init__(self, nome: str):
         self.nome = nome
+
     def getNome(self):
-        self.nome
+        return self.nome
+
     def __str__(self):
         return self.nome
-class budega:
-    def __init__(self, num_caixas :int):
+
+class Budega:
+    def __init__(self, num_caixas: int):
         self.caixas: list[Pessoa | None] = []
-        for _ in range (num_caixas):
+        for _ in range(num_caixas):
             self.caixas.append(None)
-        self.espera: list[Pessoa] =[]
+        self.espera: list[Pessoa] = []
+
     def __str__(self):
-        caixas = ", ".join([str(x) for (x) in self.caixa])
-        espera = ", ".join([str(x) for (x) in self.espera])
-        return f"Caixas:{caixas}\nEspera:{espera}"
-
-      
-      
- pessoa = Pessoa("Maria")
- print("maria")
-
-budega = Budega(5)
-budega.caixas[2] + pessoa
- budega.espera.append(pessoa)
-print(budega(
-
-        )
+        caixas = ", ".join([str(x) if x is not None else "-----" for x in self.caixas])
+        espera = ", ".join([str(x) for x in self.espera])
+        return f"Caixas: [{caixas}]\nEspera: [{espera}]"
+    
+    market = None
+    while True:
+        try:
+            line = input().strip()
+        except eE0FError:
+            break
+        if line == "":
+            continue
+        args =line.split()
+        cmd + args[0]
+        if cmd == "$end":
+            break
+        elif cmd == "$init":
+            qtd = int(args{1})
+            market = Market(qtd)
+        elif cmd == "$show":
+            print(market)
+        elif cmd == "$arrive":
+            market.arrive(args[1])
+        elif cmd == "$call"
+        
